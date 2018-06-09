e40c1c6f937dc01829a97272f99add7c9bcddbf4
Michael Prather <mprather1@gmail.com>
2018-06-09 19:31:44 +0000

    refactored Navbar.jsx

--- src/frontend/components/Navbar.jsx
+++ src/frontend/components/Navbar.jsx
@@ -1,25 +1,27 @@
-export default class Navbar extends React.Component {
-  render () {
-    const { changeActiveTab, modal, active } = this.props
+import PropTypes from 'prop-types'
 
-    return (
-      <nav>
+const Navbar = ({ changeActiveTab, modal, active }) =>
+  <nav>
+    <li onClick={() => { changeActiveTab('#/') }}><a className={active === '#/' || active === '' ? 'active' : null} href='#/'>home</a></li>
 
-        <li onClick={() => { changeActiveTab('#/') }}><a className={active === '#/' || active === '' ? 'active' : null} href='#/'>home</a></li>
+    <li onClick={() => { changeActiveTab('#/users') }}><a className={active === '#/users' ? 'active' : null} href='#/users'>users</a></li>
 
-        <li onClick={() => { changeActiveTab('#/users') }}><a className={active === '#/users' ? 'active' : null} href='#/users'>users</a></li>
+    <li onClick={() => { changeActiveTab('#/devices') }}><a className={active === '#/devices' ? 'active' : null} href='#/devices'>devices</a></li>
 
-        <li onClick={() => { changeActiveTab('#/devices') }}><a className={active === '#/devices' ? 'active' : null} href='#/devices'>devices</a></li>
+    <div className='dropdown'>
+      <button className='dropbtn'>actions &middot;</button>
+      <div className='dropdown-content'>
+        <a onClick={modal} id='create-new'>Create New</a>
+        <a id='login' href='#/login'>Login</a>
+        <a id='logout'>Logout</a>
+      </div>
+    </div>
+  </nav>
 
-        <div className='dropdown'>
-          <button className='dropbtn'>actions &middot;</button>
-          <div className='dropdown-content'>
-            <a onClick={modal} id='create-new'>Create New</a>
-            <a id='login' href='#/login'>Login</a>
-            <a id='logout'>Logout</a>
-          </div>
-        </div>
-      </nav>
-    )
-  }
+Navbar.propTypes = {
+  changeActiveTab: PropTypes.func,
+  modal: PropTypes.func,
+  active: PropTypes.string
 }
+
+export default Navbar
